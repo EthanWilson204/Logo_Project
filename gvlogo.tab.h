8ec050c7d98e22747c6229ed381d658035276330
Ethan Wilson <ehwilson.business@gmail.com>
2024-03-19 00:14:59 -0400

    created a gvlogo.tab.h file that includes the definitions of the gvlogo.y file

---
+++ gvlogo.tab.h
@@ -0,0 +1,28 @@
+#ifndef	_GVLOGO_TAB_H_
+#define _GVLOGO_TAB_H_
+
+#define WIDTH 640
+#define HEIGHT 480
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
+
+int yylex(void);
+int yyerror(const char* s);
+void startup();
+int run(void* data);
+void prompt();
+void penup();
+void pendown();
+void move(int num);
+void turn(int dir);
+void output(const char* s);
+void change_color(int r, int g, int b);
+void clear();
+void save(const char* path);
+void shutdown();
+
+#endif
